0f23af01ef4ade8d0818663f9cda40797cc04b9d
reshanadila <151610245@smkassalaambandung.sch.id>
2017-08-07 17:07:47 +0700

    login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h2><center>Sistem Informasi Bahan Bangunan</center></h2></div>
+
+                <div class="panel-body">
+                <h3>Selamat Datang</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-md-8 col-md-offset-2">
+<div class="panel panel-default">
+<div class="panel-heading">Welcome</div>
+<div class="panel-body">
+<h1><center>Selamat Datang Di Sistem Informasi Bahan Bangunan</center></h1>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
